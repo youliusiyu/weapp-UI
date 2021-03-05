ec4382d6cfaac90d25d275c00b0e3c8f85925e73
everglow <332283588@qq.com>
2021-03-05 17:16:50 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,166 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area" @click="toPage">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="navtab">
+			<view class="list" @click="change" id='0'>怡宝</view>
+			<view class="list" @click="change" id='1'>农夫山泉</view>
+			<view class="list" @click="change" id='2'>娃哈哈</view>
+			<view class="active_line" :style="{transform: active.transform }"></view>
+		</view>
+		<view class="box">
+			<view class="title" :style="{height: h}" style="transition: height 0.3s;" >
+				<view class="head" @click="changeH">HEAD 1</view>
+				<view class="shou">
+					<view class="feng">TO</view>
+					<view class="feng">SAY</view>
+					<view class="feng">HELLO</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				active:{
+					transform: 'translateX(60px) translateX(-50%)'
+				},
+				h:'50px'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toPage(){
+				uni.navigateTo({
+					url:'../newpage/newpage'
+				})
+			},
+			change(e){
+				console.log(e)
+				let id = e.currentTarget.id
+				switch(parseInt(id)){
+					case 0 :
+						this.active.transform = 'translateX(60px) translateX(-50%)'
+						break;
+					case 1 :
+						this.active.transform = 'translateX(180px) translateX(-50%)'
+						break;
+					case 2 :
+						this.active.transform = 'translateX(300px) translateX(-50%)'
+						break;
+				}
+			},
+			changeH(){
+				if(this.h == '50px'){
+					this.h = '200px'
+				}else {
+					this.h = '50px'
+				}
+			}
+		}
+		
+	}
+</script>
+
+<style>
+	.box {
+		width: 690rpx;
+		padding: 0 30rpx;
+		display: flex;
+		flex-direction: column;
+	}
+	.title {
+		overflow:hidden;
+		/* transition: height 0.3; */
+		height: 200px;
+		position: relative;
+	}
+	.head {
+		height: 50px;
+		border-top: 0.5rpx solid #1989FA;
+	}
+	.shou {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		overflow:auto;
+	}
+	.feng {
+		line-height: 2;
+		background-color: #f4f5f6;
+	}
+	.feng:after {
+		position: absolute;
+		    right: 0;
+		    left: 0;
+		    height: 1px;
+		    content: '';
+		    -webkit-transform: scaleY(.5);
+		    transform: scaleY(.5);
+		    background-color: #c8c7cc;
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.navtab {
+		width: 720rpx;
+		padding: 0 15rpx;
+		display: flex;
+		flex-direction: row;
+		position: relative;
+	}
+	
+	.list {
+		position: relative;
+		display: flex;
+		flex: 1;
+		line-height: 2.5;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.active_line {
+		position: absolute;
+		    bottom: 0;
+		    z-index: 1;
+		    width: 40px;
+		    height: 3px;
+		    background-color: #ee0a24;
+		    border-radius: 3px;
+			transition-duration:.3s;
+	}
+</style>
---
+++ pages/newpage/newpage.vue
@@ -0,0 +1,280 @@
+<template>
+	<view class="content">
+		<view class="head">
+			<view class="swiper_first">
+				<swiper class="swiper" style="height:100vh" 
+				:vertical="vertical" 
+				:autoplay="autoplay" 
+				:interval="interval" 
+				:duration="duration" >
+					<swiper-item>
+						<view class="item" style="height:100vh">
+							<image src="../../images/sb1.jpg" class="item_1_img" mode="aspectFill"></image>
+						</view>
+						<view class="bot">
+							<image src="../../images/bot-arrow.png" class="bot_arrow"></image>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="item" style="height: 100vh;">
+							<image src="../../images/p1.jpg" class="item_1_img" mode="aspectFill"></image>
+						</view>
+						<view class="hov">
+							<image src="../../images/jl.png" class="hov_img"></image>
+						</view>
+						<view class="bot">
+							<image src="../../images/bot-arrow.png" class="bot_arrow"></image>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="item" style="height: 100vh;">
+							<image src="../../images/sb1.jpg" class="item_1_img" mode="aspectFill"></image>
+						</view>
+						<view class="hov">
+							<image src="../../images/jl.png" class="hov_img"></image>
+						</view>
+						<view class="bot">
+							<image src="../../images/bot-arrow.png" class="bot_arrow"></image>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="item believe" style="height: 100vh;">
+							<view class="top-nav"></view>
+							<view class="center-ce">
+								<view class="center-ce-left">
+									<view class="left-up">
+										<image src="../../images/channel/1.png" mode="aspectFill"></image>
+									</view>
+									<view class="left-down">
+										<image src="../../images/jl.png"></image>
+									</view>
+								</view>
+								<view class="center-ce-right"></view>
+							</view>
+							<view class="bottom-arrow"></view>
+						</view>
+						<view class="bot">
+							<image src="../../images/bot-arrow.png" class="bot_arrow"></image>
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="item" style="height:100vh; position: relative;">
+							<view class="kole">
+								<text class="f_1">可口可乐{{textarea}}</text>
+							</view>
+							<view class="imgbg">
+								<image class="bg" src="../../images/w3.png" mode="aspectFill"></image>
+								<view class="create">生成海报</view>
+							</view>
+							<view class="text-area">
+								<view class="areabox">
+									<view class="intext">可口可乐</view>
+									<textarea class="textarea" v-model="textarea"></textarea>
+									<!-- <view class="create">生成海报</view> -->
+								</view>
+							</view>
+							<!-- <view class="create">生成海报</view> -->
+						</view>
+					</swiper-item>
+				</swiper>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				vertical: true,
+				autoplay: false,
+				interval: 2000,
+				duration: 300,
+				screenHeight:'100vh',
+				textarea:''
+			}
+		},
+		onLoad() {
+			
+		},
+		onShow() {
+			
+		},
+		onReady() {
+			this.getViewHeight()
+		},
+		methods: {
+			getViewHeight(){
+				uni.getSystemInfo({
+					success(res) {
+						// console.log(res)
+						this.screenHeight = res.screenHeight*2 + 'rpx'
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	textarea:focus, input:focus{
+	    outline: none;
+	}
+	.content {
+		background: black;
+		margin: 0;
+		padding: 0;
+	}
+	.swiper {
+		font-size: 18px;
+		
+	}
+	.swiper_first {
+		width: 100%;
+		background: black;
+	}
+	.item {
+		background-color: #9d1f2a;
+	}
+	
+	.item_1_img {
+		width: 100%;
+		height: 100vh;
+	}
+	.hov {
+		position: absolute;
+		left: 0;
+		top: 500rpx;
+		width: 70rpx;
+		height: 238rpx;
+		.hov_img {
+			width: 70rpx;
+			height: 238rpx;
+		}
+	}
+	
+	.bot {
+		width: 100%;
+		position: absolute;
+		bottom: 0;
+		display: flex;
+		height: 80rpx;
+		align-items: center;
+		justify-content: center;
+		.bot_arrow {
+			width: 70rpx;
+			height: 40rpx;
+		}
+	}
+	
+	.believe {
+		display: flex;
+		flex-direction: column;
+		.top-nav {
+			height: 100rpx;
+			background-color: black;
+		}
+		.center-ce {
+			flex: 1;
+			display: flex;
+			flex-direction: row;
+			.center-ce-left {
+				width: 80rpx;
+				background-color: black;
+				display: flex;
+				flex-direction: column;
+				.left-up {
+					flex: 1;
+					width: 80rpx;
+				}
+				.left-up image {
+					margin-top: 50rpx;
+					width: 80rpx;
+					height: 400rpx;
+				}
+				.left-down {
+					display: flex;
+					flex: 1;
+					width: 80rpx;
+					align-items: center;
+					justify-content: center;
+				}
+				.left-down image {
+					width: 70rpx;
+					height: 238rpx;
+				}
+			}
+			.center-ce-right {
+				flex: 1;
+			}
+		}
+		.bottom-arrow {
+			height: 100rpx;
+			background-color: black;
+		}
+		
+	}
+	
+	.kole {
+		width: 690rpx;
+		padding: 300rpx 30rpx 30rpx 30rpx;
+		.f_1 {
+			font-size: 30px;
+			font-weight: bold;
+			color: white;
+		}
+	}
+	.imgbg {
+		position: absolute;
+		width: 550rpx;
+		height: 720rpx;
+		left: 100rpx;
+		.bg {
+			width: 550rpx;
+			height: 720rpx;
+		}
+	}
+	.text-area {
+		width: 550rpx;
+		padding: 0 100rpx;
+		margin-top: 160rpx;
+		.areabox{
+			position: relative;
+			.textarea {
+				width: 510rpx;
+				background-color: rgba(255,255,255,0.5);
+				height: 200rpx;
+				text-indent: 80px;
+				font-size: 16px;
+				line-height: 1.5;
+				color: white;
+				padding: 20rpx;
+			}
+			.intext {
+				position: absolute;
+				left: 20rpx;
+				top: 20rpx;
+				color: white;
+				line-height: 1.5;
+				font-size: 16px;
+				display: block;
+			}
+		}
+		
+	}
+	
+	.create {
+		width: 550rpx;
+		// margin: 0 100rpx;
+		text-align: center;
+		color: #9d1f2a;
+		// background-color: white;
+		position: absolute;
+		bottom: 0;
+		line-height: 1.8;
+		letter-spacing:20rpx;
+		font-weight: bold;
+		font: 32px;
+	}
+</style>
