ed8f8a3b5c7780a2db2bec9975fb42767fe43f9b
everglow <332283588@qq.com>
2021-03-26 16:59:55 +0800

    init

--- pages/spritejs/spritejs.vue
+++ pages/spritejs/spritejs.vue
@@ -22,13 +22,26 @@
 			}
 		},
 		onLoad() {
-			
+			const srcParts = [
+			  [0, 0, 190, 268],
+			  [0, 269, 190, 268],
+			  [191, 0, 190, 268],
+			  [191, 269, 190, 268],
+			];
+			for(let i = 0; i < 2; i++) {
+			  for(let j = 0; j < 2; j++) {
+			    const sourceRect = srcParts[i * 2 + j];
+			    const x = 360 + i * 200;
+			    const y = j * 278;
+				console.log(sourceRect,x,y)
+			  }
+			}
 		},
 		async onReady() {
 			let winfo = await this.getWindowInfo()
 			let i = await this.init()
 			let pre = await this.preloadTexture('static/images/loading/cover.png')
-			this.drawInit()
+			this.drawBtn()
 		},
 		async onShow() {
 			
@@ -95,6 +108,110 @@
 				// 	texture: 'static/images/loading/hair-1.png',
 				// });
 				// this.layer.append(this.hair);
+			},
+			async drawBtn(){
+				const robot1 = new Sprite({
+					pos:[0,480],
+					size:[750,90],
+					scale: [0.5,0.5],
+					sourceRect:[0,970 ,750,90],
+					texture: 'static/images/question/1/1-1.png',
+				});
+				const robot2 = new Sprite({
+					texture: 'static/images/question/1/2-1.png',
+					pos:[0,545],
+					size:[750,90],
+					scale: [0.5,0.5],
+					sourceRect:[0,1090 ,750,90],
+				});
+				
+				const robot3 = new Sprite({
+					texture: 'static/images/question/1/3-1.png',
+					pos:[0,610], 
+					size:[750,90],
+					scale: [0.5,0.5],
+					sourceRect:[0,1220 ,750,90],
+				});
+				
+				// btn平移
+				// robot1.transition(0.5).attr({
+				//   x: (x) => x + 750
+				// }).then(() => {
+				// 	robot2.transition(0.5).attr({
+				// 	  x: (x) => x + 750
+				// 	}).then(() => {
+				// 		robot3.transition(0.5).attr({
+				// 		  x: (x) => x + 750
+				// 		}).then(() => {
+				// 			return true
+				// 		})
+				// 	})
+				// })
+				
+				robot1.addEventListener('mouseenter', async (e) => {
+					console.log('mouseenter1',e)
+				})
+				robot2.addEventListener('mouseenter', async (e) => {
+					console.log('mouseenter2',e)
+				})
+				robot3.addEventListener('mouseenter', async (e) => {
+					console.log('mouseenter3',e)
+				})
+				this.layer.append(robot1,robot2,robot3);
+				
+				this.question1(robot1,robot2,robot3)
+				// this.question2(robot1,robot2,robot3)
+			},
+			/**
+			 * btn平移
+			 * */
+			question1(robot1,robot2,robot3){
+				robot1.animate([
+					{translate: [-750, 0]},
+					{translate: [0, 0]},
+				], {
+				  duration: 500,
+				  easing: 'ease-in-out',
+				  // direction: 'alternate',
+				});
+			},
+			/**
+			 * btn图片重复显示
+			 * */
+			question2(robot1,robot2,robot3){
+				robot1.animate([
+					{texture: 'static/images/question/1/1-1.png'},
+					{texture: 'static/images/question/1/1-2.png'},
+					{texture: 'static/images/question/1/1-3.png'},
+					{texture: 'static/images/question/1/1-4.png'},
+				], {
+				  duration: 400,
+				  easing: 'step-end',
+				  fill: 'forwards',
+				  iterations: Infinity,
+				});
+				robot2.animate([
+					{texture: 'static/images/question/1/2-1.png'},
+					{texture: 'static/images/question/1/2-2.png'},
+					{texture: 'static/images/question/1/2-3.png'},
+					{texture: 'static/images/question/1/2-4.png'},
+				], {
+				  duration: 400,
+				  easing: 'step-end',
+				  fill: 'forwards',
+				  iterations: Infinity,
+				});
+				robot3.animate([
+					{texture: 'static/images/question/1/3-1.png'},
+					{texture: 'static/images/question/1/3-2.png'},
+					{texture: 'static/images/question/1/3-3.png'},
+					{texture: 'static/images/question/1/3-4.png'},
+				], {
+				  duration: 400,
+				  easing: 'step-end',
+				  fill: 'forwards',
+				  iterations: Infinity,
+				});
 			},
 			/** 
 			 * 异步预加载图片资源
